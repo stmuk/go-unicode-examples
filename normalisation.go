2159c6da50207c1512a604d7f9b186967ee93884
Steve Mynott <steve.mynott@gmail.com>
2018-04-03 15:31:05 +0100

    improve normalisation output

--- normalisation.go
+++ normalisation.go
@@ -10,16 +10,21 @@ func main() {
 	one := "e\u0301"
 	two := "\u00e9"
 
+	fmt.Printf("bytes %x\n", one)
+
 	fmt.Printf("one: %q two: %q\n", one, two)
 
 	if one == two {
 		fmt.Println("equals")
 	} else {
-		fmt.Println("not equals")
+		fmt.Printf("%x not equals %x\n", one, two)
 	}
 
-	if norm.NFC.String(one) == norm.NFC.String(two) {
-		fmt.Println("equals")
+	onen := norm.NFC.String(one)
+	twon := norm.NFC.String(two)
+
+	if onen == twon {
+		fmt.Printf("%x equals %x\n", onen, twon)
 	} else {
 		fmt.Println("not equals")
 	}
