bca3937b2865a16acc81fba4487bdb83e3a5732f
Steve Mynott <steve.mynott@gmail.com>
2018-04-03 16:06:43 +0100

    checkpoint

---
+++ stack.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// START OMIT
+
+	fraction := "⅜" // VULGAR FRACTION THREE EIGHTHS
+
+	fmt.Printf("codepoint: %x", []rune(fraction))
+
+	fmt.Printf("UTF-8 hex bytes: %x\n", fraction)
+
+	// END OMIT
+}
